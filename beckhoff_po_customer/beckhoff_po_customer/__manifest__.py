61077e97d8a5e704c076496365d0fefe3917a846
janbec <beckhoff.jan@gmail.com>
2017-06-25 22:25:59 +0200

    Add files via upload

---
+++ beckhoff_po_customer/beckhoff_po_customer/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "beckhoff_po_customer",
+
+    'summary': """
+        Adding field customer and salesperson to Purchase Order""",
+
+    'description': """
+        This App will create the fields Salesperson and Customer to be assigned to a Purchase Order. The Purchase Orders will be shown in Partner Form.
+    """,
+
+    'author': "Jan Beckhoff",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['purchase'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ beckhoff_po_customer/beckhoff_po_customer/models/models.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class CustomerToPo(models.Model):
+    _inherit = 'purchase.order'
+
+    customer = fields.Many2one('res.partner', 'Customer', track_visibility='OnChange')
+    salesperson = fields.Many2one('res.users','Salesperson', track_visibility='OnChange')
+
+    @api.multi
+    @api.depends('name', 'partner_ref')
+    def name_get(self):
+        result = []
+        for po in self:
+            name = po.name
+            if po.partner_ref:
+                name += ' ('+po.partner_ref+')'
+            if po.amount_total:
+                name += ' ' #+ formatLang(self.env,  po.amount_total, currency_obj=po.currency_id)
+            result.append((po.id, name))
+        return result
+
+class CustomerPoCounter(models.Model):
+    _inherit = 'res.partner'
+
+    customer_purchase_order = fields.Integer(compute='_purchase_invoice_count', string='# of Purchase Orders')
+
+    @api.one
+    def _purchase_invoice_count(self):
+        cond = [('customer', '=', self.id)]
+        self.customer_purchase_order = len(self.env['purchase.order'].search(cond))
