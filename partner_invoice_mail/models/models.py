c92266ea348aa19ec8eea0dfd59ca856bc4a3d59
janbec <beckhoff.jan@gmail.com>
2017-10-31 17:44:44 +0100

    Cleanup + readonly fix
    account.invoice checkbox is now readonly

--- partner_invoice_mail/models/models.py
+++ partner_invoice_mail/models/models.py
@@ -6,13 +6,6 @@ class PartnerInvoiceMail(models.Model):
      _inherit = 'res.partner'
 
      partner_invoice_mail = fields.Boolean(string="Invoices via Mail", help="If this box is checked, a notification will display on the invoice reminding the user to send the invoice via Mail.")
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
 
 class PartnerAccountInvoiceMail(models.Model):
     _inherit = 'account.invoice'
