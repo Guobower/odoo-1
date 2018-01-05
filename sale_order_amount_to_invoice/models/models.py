77b16dc4e3a4893549e70fd27710a5b59c07af0a
janbec <beckhoff.jan@gmail.com>
2018-01-05 21:42:18 +0100

    deduct down payment from amount_to_invoice
    price_tax_incl is not calculated when qty = 0

--- sale_order_amount_to_invoice/models/models.py
+++ sale_order_amount_to_invoice/models/models.py
@@ -11,6 +11,11 @@ class SaleOrderAmountToInvoice(models.Model):
     @api.depends('order_line.qty_to_invoice')
     def _calculate_amount_to_invoice(self):
         for line in self.order_line:
-            if line.product_uom_qty < line.qty_to_invoice:
-                self.amount_to_invoice -= line.price_reduce_taxinc * line.qty_to_invoice
             self.amount_to_invoice += line.price_reduce_taxinc * line.qty_to_invoice
+            if line.product_uom_qty < line.qty_to_invoice:
+                self.amount_to_invoice -= line.price_unit
+#            if line.product_uom_qty < line.qty_to_invoice:
+#                if self.partner_id.is_company = True:
+#                    self.amount_to_invoice -= (line.price_unit + price_tax) * line.qty_to_invoice
+#                else:
+#                    self.amount_to_invoice -= line.price_unit * line.qty_to_invoice
