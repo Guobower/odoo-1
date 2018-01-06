04e54add69019705773df00f49892deca070ea08
janbec <beckhoff.jan@gmail.com>
2018-01-06 16:20:22 +0100

    bug fixing-wrong field for down payment detection

--- sale_order_amount_to_invoice/models/models.py
+++ sale_order_amount_to_invoice/models/models.py
@@ -12,10 +12,10 @@ class SaleOrderAmountToInvoice(models.Model):
     def _calculate_amount_to_invoice(self):
         for line in self.order_line:
             self.amount_to_invoice += line.price_reduce_taxinc * line.qty_to_invoice
-            if line.product_uom_qty < line.qty_to_invoice:
-                self.amount_to_invoice -= line.price_unit
-#            if line.product_uom_qty < line.qty_to_invoice:
-#                if self.partner_id.is_company = True:
-#                    self.amount_to_invoice -= (line.price_unit + price_tax) * line.qty_to_invoice
-#                else:
-#                    self.amount_to_invoice -= line.price_unit * line.qty_to_invoice
+            # detect down payment or sale order lines to be refunded
+            if line.product_uom_qty < line.qty_invoiced:
+                if self.partner_id.company_type == 'company':
+                    # Add tax to price_unit, amount_total is tax inclusive
+                    self.amount_to_invoice -= (line.price_unit + line.price_tax) * line.qty_invoiced
+                else
+                    self.amount_to_invoice -= line.price_unit * line.qty_invoiced
