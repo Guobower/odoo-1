073e9faf40082559aa99687bdd7f0b27c91f81dc
janbec <beckhoff.jan@gmail.com>
2018-01-09 22:51:13 +0100

    add account.invoice support

--- order_line_absolute_discount/__manifest__.py
+++ order_line_absolute_discount/__manifest__.py
@@ -16,10 +16,10 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '10.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale'],
+    'depends': ['sale', 'account'],
 
     # always loaded
     'data': [
