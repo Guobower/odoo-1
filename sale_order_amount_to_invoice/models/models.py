2b2120496a06509e886e7a7f255df28ce89069ab
janbec <beckhoff.jan@gmail.com>
2018-01-13 12:55:56 +0100

    fix down payment company tax bug

--- sale_order_amount_to_invoice/models/models.py
+++ sale_order_amount_to_invoice/models/models.py
@@ -8,14 +8,18 @@ class SaleOrderAmountToInvoice(models.Model):
     amount_to_invoice = fields.Monetary(compute="_calculate_amount_to_invoice", store=True)
 
     @api.one
-    @api.depends('order_line.qty_to_invoice')
+    @api.depends('order_line.qty_to_invoice', 'order_line.discount')
     def _calculate_amount_to_invoice(self):
         for line in self.order_line:
+            # pricde_reduce_taxinc handles all discount/tax operations
             self.amount_to_invoice += line.price_reduce_taxinc * line.qty_to_invoice
             # detect down payment or sale order lines to be refunded
+            # extra calculation necessary because a few fields won't compute with qty = 0
             if line.product_uom_qty < line.qty_invoiced:
+                price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
                 if self.partner_id.company_type == 'company':
                     # Add tax to price_unit, amount_total is tax inclusive
-                    self.amount_to_invoice -= (line.price_unit + line.price_tax) * line.qty_invoiced
+                    tax = line.price_unit * line.tax_id.amount / 100
+                    self.amount_to_invoice -= (price + tax) * line.qty_invoiced
                 else:
-                    self.amount_to_invoice -= line.price_unit * line.qty_invoiced
+                    self.amount_to_invoice -= price * line.qty_invoiced
