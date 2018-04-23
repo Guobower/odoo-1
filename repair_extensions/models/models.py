a95661eeae51722ce62b50fb4180fbe208043e99
janbec <beckhoff.jan@gmail.com>
2018-04-23 15:08:07 +0200

    REMOVE stock move for repaired product

--- repair_extensions/models/models.py
+++ repair_extensions/models/models.py
@@ -85,6 +85,71 @@ class RepairOrder(models.Model):
         stage_ids = self.env['mrp.repair.stage'].search([])
         return stage_ids
 
+    # This is the original function from mrp.repair, overwritten to prevent stock intake through repair order
+    @api.multi
+    def action_repair_done(self):
+        """ Creates stock move for operation and stock move for final product of repair order.
+        @return: Move ids of final products
+        """
+        if self.filtered(lambda repair: not repair.repaired):
+            raise UserError(_("Repair must be repaired in order to make the product moves."))
+        res = {}
+        Move = self.env['stock.move']
+        for repair in self:
+            moves = self.env['stock.move']
+            for operation in repair.operations:
+                move = Move.create({
+                    'name': repair.name,
+                    'product_id': operation.product_id.id,
+                    'product_uom_qty': operation.product_uom_qty,
+                    'product_uom': operation.product_uom.id,
+                    'partner_id': repair.address_id.id,
+                    'location_id': operation.location_id.id,
+                    'location_dest_id': operation.location_dest_id.id,
+                    'move_line_ids': [(0, 0, {'product_id': operation.product_id.id,
+                                           'lot_id': operation.lot_id.id,
+                                           'product_uom_qty': 0,  # bypass reservation here
+                                           'product_uom_id': operation.product_uom.id,
+                                           'qty_done': operation.product_uom_qty,
+                                           'package_id': False,
+                                           'result_package_id': False,
+                                           'location_id': operation.location_id.id, #TODO: owner stuff
+                                           'location_dest_id': operation.location_dest_id.id,})],
+                    'repair_id': repair.id,
+                    'origin': repair.name,
+                })
+                moves |= move
+                operation.write({'move_id': move.id, 'state': 'done'})
+            """
+            move = Move.create({
+                'name': repair.name,
+                'product_id': repair.product_id.id,
+                'product_uom': repair.product_uom.id or repair.product_id.uom_id.id,
+                'product_uom_qty': repair.product_qty,
+                'partner_id': repair.address_id.id,
+                'location_id': repair.location_id.id,
+                'location_dest_id': repair.location_dest_id.id,
+                'move_line_ids': [(0, 0, {'product_id': repair.product_id.id,
+                                           'lot_id': repair.lot_id.id,
+                                           'product_uom_qty': 0,  # bypass reservation here
+                                           'product_uom_id': repair.product_uom.id or repair.product_id.uom_id.id,
+                                           'qty_done': repair.product_qty,
+                                           'package_id': False,
+                                           'result_package_id': False,
+                                           'location_id': repair.location_id.id, #TODO: owner stuff
+                                           'location_dest_id': repair.location_dest_id.id,})],
+                'repair_id': repair.id,
+                'origin': repair.name,
+            })
+            consumed_lines = moves.mapped('move_line_ids')
+            produced_lines = move.move_line_ids
+            moves |= move
+            moves._action_done()
+            produced_lines.write({'consume_line_ids': [(6, 0, consumed_lines.ids)]})
+            res[repair.id] = move.id
+            """
+        return res
+
     class RepairStage(models.Model):
         _name = 'mrp.repair.stage'
         _description = 'Stage'
