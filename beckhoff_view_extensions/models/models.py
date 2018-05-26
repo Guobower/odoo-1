5314c6cbe6a9045fcc6f047f8dfa3975219436c9
janbec <beckhoff.jan@gmail.com>
2018-05-26 14:41:49 +0200

    [IMP] track visibility of active on product and partner

--- beckhoff_view_extensions/models/models.py
+++ beckhoff_view_extensions/models/models.py
@@ -8,6 +8,7 @@ class beckhoff_view_extensions(models.Model):
     list_price = fields.Float(track_visibility='OnChange')
     name = fields.Char(track_visibility='OnChange')
     barcode = fields.Char(track_visibility='OnChange')
+    active = fields.Boolean(track_visibility='OnChange')
 
 class ProductTemplateChangeTrack(models.Model):
     _inherit = 'product.template'
@@ -15,6 +16,7 @@ class ProductTemplateChangeTrack(models.Model):
     list_price = fields.Float(track_visibility='OnChange')
     name = fields.Char(track_visibility='OnChange')
     barcode = fields.Char(track_visibility='OnChange')
+    active = fields.Boolean(track_visibility='OnChange')
 
     description_sale = fields.Text(copy=False)
     description_purchase = fields.Text(copy=False)
@@ -29,6 +31,7 @@ class PartnerChangeData(models.Model):
     email = fields.Char(track_visibility='OnChange')
     city = fields.Char(track_visibility='OnChange')
     street = fields.Char(track_visibility='OnChange')
+    active = fields.Boolean(track_visibility='OnChange')
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice.line'
