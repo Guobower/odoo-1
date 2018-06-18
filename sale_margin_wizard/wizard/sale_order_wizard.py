a3d5dd4ec005bac4bad96414c0d1483dc62cd3af
janbec <beckhoff.jan@gmail.com>
2018-06-18 22:18:45 +0200

    [ADD] Support for mass discount editing

--- sale_margin_wizard/wizard/sale_order_wizard.py
+++ sale_margin_wizard/wizard/sale_order_wizard.py
@@ -1,11 +1,20 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import models, fields, api, _
+from odoo.addons import decimal_precision as dp
+from odoo.exceptions import ValidationError
 
 
 class SaleOrderMarginWizard(models.TransientModel):
     _name = 'sale.order.margin.wizard'
     _description = 'View and manipulate prices and margins per sale order line.'
+
+    def discount_mode_selection(self):
+        return [
+            ('price', 'Price'),
+            ('relative', 'Relative (%)'),
+            ('absolute', 'Absolute (Euro)'),
+            ]
 
     order_line_ids = fields.One2many('sale.order.margin.line.wizard', 'order_id')
     sale_order_id = fields.Many2one('sale.order', string='Sale Order')
@@ -16,10 +25,14 @@ class SaleOrderMarginWizard(models.TransientModel):
     company_id = fields.Many2one('res.company', string='Company', required=True, index=True,
                                  default=lambda self: self.env.user.company_id.id)
     pricelist_id = fields.Many2one('product.pricelist', string='Pricelist')
+    margin_target = fields.Float(string='Target Margin', help="Computes the price for each line to have the entered margin.")
+    price_target = fields.Float(string='Target Price', help='Discounts all lines in relation to their price.')
+    discount_mode = fields.Selection(string='Discount Mode', selection='discount_mode_selection')
 
-    amount_total = fields.Float(string='Amount Total', compute="compute_total")
-    margin_total = fields.Float(string='Margin Total', compute="compute_total")
-    margin_percent_total = fields.Float(string='Margin Total (%)', compute="compute_total")
+    amount_total = fields.Float(string='Amount Total', compute="compute_total", digits=dp.get_precision('Product Price'))
+    margin_total = fields.Float(string='Margin Total', compute="compute_total", digits=dp.get_precision('Product Price'))
+    margin_percent_total = fields.Float(string='Margin Total (%)', compute="compute_total", digits=dp.get_precision('Discount'))
+
 
     def create_so_lines(self, s_order):
         for line in s_order.order_line:
@@ -34,19 +47,105 @@ class SaleOrderMarginWizard(models.TransientModel):
                 'discount': line.discount,
                 'discount_absolute': line.discount_absolute,
                 'taxes_id': taxes,
+                'sale_order_line_id': line.id,
             })
             self.order_line_ids += so_line
 
-    @api.depends('order_line_ids.margin')
+
+    def reset_so_lines(self):
+        for line in self.order_line_ids:
+            line.price_unit = line.sale_order_line_id.price_unit
+            line.cost_unit = line.sale_order_line_id.purchase_price
+            line.discount = line.sale_order_line_id.discount
+            line.discount_absolute = line.sale_order_line_id.discount_absolute
+        return {"type": "ir.actions.do_nothing"}
+
+
+    def restore_so_lines_prices(self):
+        for line in self.order_line_ids:
+            line.write({
+                'discount': 0,
+                'discount_absolute': 0,
+                'price_unit': line.product_id.lst_price,
+                'cost_unit':line.product_id.standard_price,
+            })
+        return {"type": "ir.actions.do_nothing"}
+
+
+    @api.depends('order_line_ids.discount', 'order_line_ids.discount_absolute', 'order_line_ids.price_unit', 'order_line_ids.cost_unit')
     def compute_total(self):
         for line in self.order_line_ids:
             self.amount_total += line.price_discounted * line.product_uom_qty
             self.margin_total += line.margin * line.product_uom_qty
         self.margin_percent_total = self.margin_total / self.amount_total * 100
 
-    #TODO: create function for manipulating prices and write them to SO
+
     def manipulate_prices(self):
-        return True
+        for line in self.order_line_ids:
+            line.sale_order_line_id.discount =  line.discount
+            line.sale_order_line_id.discount_absolute = line.discount_absolute
+            line.sale_order_line_id.price_unit = line.price_unit
+            line.sale_order_line_id.purchase_price = line.cost_unit
+
+
+    # TODO: should work with gross/net unit prices
+    @api.onchange('discount_mode')
+    def compute_target_margin(self):
+        if self.margin_target < 0 or self.margin_target > 100:
+            raise ValidationError("Margin must be between 0 and 100")
+        self.reset_so_lines()
+        if self.margin_target > 0:
+            for line in self.order_line_ids:
+                line.discount = 0
+                line.discount_absolute = 0
+                selling_price = line.cost_unit / ((100 - self.margin_target) / 100)
+                if self.discount_mode == 'relative':
+                    line.discount = (line.price_unit - selling_price) / line.price_unit * 100
+                if self.discount_mode == 'absolute':
+                    line.discount_absolute = line.price_unit - selling_price
+                if self.discount_mode == 'price':
+                    line.price_unit = selling_price
+
+    @api.onchange('discount_mode')
+    def compute_target_price(self):
+        if self.price_target > 0:
+            self.reset_so_lines()
+            for line in self.order_line_ids:
+                line.discount = 0
+                line.discount_absolute = 0
+            price_target_rel = (1 - self.price_target / self.amount_total)
+            for line in self.order_line_ids:
+                if self.discount_mode == 'relative':
+                    line.discount = price_target_rel * 100
+                if self.discount_mode == 'absolute':
+                    line.discount_absolute = line.price_unit * price_target_rel
+                if self.discount_mode == 'price':
+                    line.price_unit = line.price_unit - line.price_unit * price_target_rel
+
+    # TODO Fix me
+    @api.onchange('discount_mode')
+    def compute_discount_mode(self):
+        # re-compute the discount into another discount model
+        if self.margin_target == 0 and self.price_target == 0:
+            if self.discount_mode == 'relative':
+                for line in self.order_line_ids:
+                    if line.discount_absolute > 0:
+                        line.discount = line.discount_absolute / line.price_unit * 100
+                        line.discount_absolute = 0
+            if self.discount_mode == 'absolute':
+                for line in self.order_line_ids:
+                    if line.discount > 0:
+                        line.discount_absolute = line.discount * line.price_unit
+                        line.discount = 0
+
+
+    @api.onchange('discount_mode')
+    def reset_target_fields(self):
+        if not self.discount_mode:
+            self.margin_target = 0
+            self.price_target = 0
+
+
 
 class SaleOrderMarginLineWizard(models.TransientModel):
     _name = 'sale.order.margin.line.wizard'
@@ -55,33 +154,36 @@ class SaleOrderMarginLineWizard(models.TransientModel):
     product_id = fields.Many2one('product.product', string='Product')
     section = fields.Many2one('sale.layout_category', string='Section')
     product_uom_qty = fields.Float(string='Quantity')
-    price_unit = fields.Float(string='Unit Price')
-    cost_unit = fields.Float(string='Unit Cost')
-    discount = fields.Float(string='Discount (%)')
+    price_unit = fields.Float(string='Unit Price', digits=dp.get_precision('Product Price'))
+    cost_unit = fields.Float(string='Unit Cost', digits=dp.get_precision('Product Price'))
+    discount = fields.Float(string='Discount (%)', digits=dp.get_precision('Discount'))
     discount_absolute = fields.Float(string='Discount (€)')
     taxes_id = fields.Many2many('account.tax', string='Taxes', domain=['|', ('active', '=', False), ('active', '=', True)])
     order_id = fields.Many2one('sale.order.margin.wizard', string='Order Reference', index=True, required=True, ondelete='cascade')
     amount_tax = fields.Float(string='Tax Amount')
+    sale_order_line_id = fields.Many2one('sale.order.line', string="Sale Order Line ID")
 
-    margin = fields.Float(string='Margin', compute="compute_margin")
-    margin_percent = fields.Float(string='Margin (%)', compute="compute_margin")
-    price_discounted = fields.Float(string='Discounted Price', compute="compute_margin")
+    margin = fields.Float(string='Margin', compute="compute_margin", digits=dp.get_precision('Product Price'))
+    margin_percent = fields.Float(string='Margin (%)', compute="compute_margin", digits=dp.get_precision('Discount'))
+    price_discounted = fields.Float(string='Discounted Price', compute="compute_margin", digits=dp.get_precision('Product Price'))
 
     @api.one
     @api.depends('price_unit', 'cost_unit', 'discount', 'discount_absolute')
     def compute_margin(self):
         #TODO: FIX Tax computation
-        selling_price = self.price_unit
-        if self.discount:
-            selling_price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
         if self.discount_absolute:
             selling_price = self.price_unit - self.discount_absolute
+        else:
+            selling_price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
         taxes = self.taxes_id.compute_all(selling_price, self.order_id.currency_id, self.product_uom_qty, product=self.product_id, partner=self.order_id.sale_order_id.partner_shipping_id)
         self.amount_tax = sum(t.get('amount', 0.0) for t in taxes.get('taxes', []) if t.price_include == True)
+        # If no fiscal position, the unit price is in gross and the amount_tax needs to be considered for margin computation
         if not self.order_id.sale_order_id.fiscal_position_id:
             self.margin = selling_price  - self.cost_unit - self.amount_tax
-            self.margin_percent = self.margin / selling_price * 100
+            if selling_price:
+                self.margin_percent = self.margin / selling_price * 100
         else:
             self.margin = selling_price - self.cost_unit
-            self.margin_percent = self.margin / selling_price * 100
+            if selling_price:
+                self.margin_percent = self.margin / selling_price * 100
         self.price_discounted = selling_price
